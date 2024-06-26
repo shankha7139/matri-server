3a0f66dc8f520ea4f7d2d1574b21e3ea6ba4745f
Shankharishi Khan <81064200+shankha7139@users.noreply.github.com>
2024-06-26 14:49:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const upload = multer({
 });
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://matri-new.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
